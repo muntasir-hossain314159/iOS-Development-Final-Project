8ae4b4b256ad6e04aefff3f1dca4900fb82f315f
Muntasir Hossain <ahoss1@unh.newhaven.edu>
2022-04-03 02:33:59 -0400

    Login Page

---
+++ Travelers/Pods/Target Support Files/Pods-TravelersTests/Pods-TravelersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravelersTests : NSObject
+@end
+@implementation PodsDummy_Pods_TravelersTests
+@end
--- Travelers/Travelers/AppDelegate.swift
+++ Travelers/Travelers/AppDelegate.swift
@@ -5,17 +5,29 @@
 //  Created by Hossain, Ahmed Muntasir  on 4/2/22.
 //
 
+
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+    
+        FirebaseApp.configure()
+        
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
