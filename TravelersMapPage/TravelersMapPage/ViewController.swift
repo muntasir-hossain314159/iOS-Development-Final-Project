89131d0d5aac0f1fe19f1a7f2575f43d5a0d3b6d
Ryan Horgan <70227937+RyeBreadAndButter@users.noreply.github.com>
2022-04-26 16:26:03 -0400

    Add files via upload

---
+++ TravelersMapPage/TravelersMapPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelersMapPage
+//
+//  Created by Ryan Horgan on 4/26/22.
+//
+
+import UIKit
+import MapKit
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
